487cc919dba570efac9f13c0eb28b653b14e3667
darthfantastic <77069508+darthfantastic@users.noreply.github.com>
2025-10-07 18:20:00 -0500

    Update anduril.h

--- hw/wurkkos/ts10/rgbaux-lowfet/anduril.h
+++ hw/wurkkos/ts10/rgbaux-lowfet/anduril.h
@@ -31,6 +31,9 @@
 // disable 3C to toggle between smooth and stepped ramping in Simple UI (can still toggle in Advanced UI)
 #undef USE_SIMPLE_UI_RAMPING_TOGGLE
 
+// enable 2 click turbo (Anduril 1 style)
+#define DEFAULT_2C_STYLE 1
+
 //set default RGB led off behavior to off
 #define RGB_LED_OFF_DEFAULT 0x00
 
